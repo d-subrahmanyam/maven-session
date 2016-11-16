2f8ad1b95c76e26a21da8528e0584cf9af85e4b0
Subrahmanyam <77027subbu@gmail.com>
2016-11-17 02:35:34 +0530

    Basic project with ant build script
    This project uses an ant build script to generate the jar as earlier
    but in a more cleaner fashion

---
+++ TranslatorWithAnt/src/com/subbu/translate/Result.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.subbu.translate;
+
+/**
+ * @author subbu
+ *
+ */
+public class Result {
+	
+	/**
+	 * Holds the http code
+	 */
+	private String code;
+	
+	/**
+	 * Holds the lang for translation
+	 */
+	private String lang;
+	
+	/**
+	 * Holds the resulting text
+	 */
+	private String[] text;
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return the lang
+	 */
+	public String getLang() {
+		return lang;
+	}
+
+	/**
+	 * @param lang the lang to set
+	 */
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		StringBuilder _result = new StringBuilder();
+		for(String word:text) {_result.append(word); }
+		return _result.toString();
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String[] text) {
+		this.text = text;
+	}
+
+}
