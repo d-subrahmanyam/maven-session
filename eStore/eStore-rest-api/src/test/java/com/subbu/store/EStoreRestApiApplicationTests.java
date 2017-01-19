9c8d44aadbe86be0f9f505d85c6847a17a68d0b8
Subrahmanyam <77027subbu@gmail.com>
2017-01-19 22:54:11 +0530

    Create estore project based on spring boot

---
+++ eStore/eStore-rest-api/src/test/java/com/subbu/store/EStoreRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.subbu.store;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EStoreRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
